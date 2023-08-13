ad9e8e8e5523e6202bd89874e7747197ee5ec23a
mark-Aryan <akj4073@gmail.com>
2023-08-13 11:59:32 +0530

    FIRST COMMIT GUI MINI PROJECT

---
+++ height_to_meter.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+
+
+def convert(feet, inches):
+    meters = feet * 0.3048 + inches * 0.0254
+    return meters
+
+
+layout = [[sg.Text("Feet:"), sg.Input(tooltip="Enter Feet", key="feet")],
+          [sg.Text("Inches:"), sg.Input(tooltip="Enter Inches", key="inches")],
+          [sg.Button("Convert"), sg.Text(key="output")]]
+
+window = sg.Window("Height to Meter Convertor", layout=layout)
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+    result = convert(feet,inches)
+    window["output"].update(value=f"{result}m", text_color="WHITE")
+
+    match event:
+        case sg.WIN_CLOSED:
+            break
