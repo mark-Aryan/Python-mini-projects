1c9b741220deb6be424db5836e963dc4cd52bf09
mark-Aryan <akj4073@gmail.com>
2023-08-15 18:13:16 +0530

    First commmit

---
+++ Web_YT_Downloader.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from pytube import YouTube
+
+
+
+def download(url):
+    youtubeObject = YouTube(url)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except Exception:
+        print("An error has occurred")
+    st.success("Download is completed successfully")
+
+
+st.title("Youtube Video Downloader created by Aryan Kumar Upadhyay")
+st.warning("For educational purpose only.")
+
+try:
+    link = st.text_input('The URL link')
+    download(link)
+except Exception as e:
+    pass
+
