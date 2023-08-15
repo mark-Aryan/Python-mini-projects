e05289cc01fef11e1ca5c39e3e8d86384264b119
mark-Aryan <akj4073@gmail.com>
2023-08-15 13:12:11 +0530

    Initial Commit

--- height_to_meterGUI.py
+++ height_to_meterGUI.py
@@ -8,20 +8,25 @@ def convert(feet, inches):
 
 layout = [[sg.Text("Feet:"), sg.Input(tooltip="Enter Feet", key="feet")],
           [sg.Text("Inches:"), sg.Input(tooltip="Enter Inches", key="inches")],
-          [sg.Button("Convert"), sg.Text(key="output")]]
+          [sg.Button("Convert"), sg.Button("Exit"), sg.Text(key="output")]]
 
 window = sg.Window("Height to Meter Convertor", layout=layout)
 
 while True:
     event, values = window.read()
-    print(event, values)
-    feet = float(values["feet"])
-    inches = float(values["inches"])
-    result = convert(feet,inches)
-    window["output"].update(value=f"{result}m", text_color="WHITE")
-
     match event:
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
+    try:
+        feet = float(values["feet"])
+        inches = float(values["inches"])
+        result = convert(feet,inches)
+        window["output"].update(value=f"{result}m", text_color="WHITE")
+
+    except ValueError:
+        sg.popup("Please provide 2 number!", title="Error")
+
 
 window.close()
